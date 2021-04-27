1754a31111d8a78ac67b18bf594ec1984ba30adf
Madelineqt <47864868+Madelineqt@users.noreply.github.com>
2021-04-27 18:46:26 +0200

    auth

--- index.js
+++ index.js
@@ -26,6 +26,28 @@ app.listen(8080, () => {
 });
 const username = process.env.USER
 const password = process.env.PASSWORD
+const jwtmiddleware = (req,res,next) => {
+    const bearerHeader = req.headers["authorization"]
+    if(typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ")
+    const bearerToken = bearer[1]
+    jwt.verify(bearerToken, 'secretkey', (err, result) => {
+        if(err) { res.send({
+            ok: false,
+            message: "Bad Auth"
+           }) }
+        else{ next() }
+        })
+    } else {
+    res.send({
+        ok: false,
+        message: "Bad Auth"
+       })
+    }
+} 
+app.get('/check', jwtmiddleware, (req,res) => {
+    res.send({ok: true,message: "Success token"})
+})
 app.post('/login', (req,res) => {   
     const p_username = req.body.username
     const p_password = req.body.password
@@ -47,29 +69,25 @@ app.post('/login', (req,res) => {
            })
        }
 })
-const jwtmiddleware = (req,res,next) => {
-    const bearerHeader = req.headers["authorization"]
-    if(typeof bearerHeader !== 'undefined') {
-    const bearer = bearerHeader.split(" ")
-    const bearerToken = bearer[1]
-    jwt.verify(bearerToken, 'secretkey', (err, result) => {
-        if(err) { res.sendStatus(403) }
-        else{ next() }
-        })
-    } else {
-    res.sendStatus(403)
-    }
-} 
-app.get('/test',jwtmiddleware, (req,res) => {
-    const date = new Date(2021,3,27 ,0,0,0,0)
+
+app.get('/test',jwtmiddleware, async (req,res) => {
+    const date = new Date(2021,3,1 ,0,0,0,0)
     const timestamp = date.getTime()
     const date2 = new Date()
     const timestamp2 = date2.getTime()
-    console.log(timestamp)
-    binanceRest.accountSnapshot({type:'SPOT', startTime:timestamp, endTime:timestamp2})
+    const btccandles = await binanceRest.klines({symbol:'BTCUSDT', interval:'1d', startTime:timestamp, endTime:timestamp2})
+    // const btcvalue = await binanceRest.tickerPrice({symbol:'BTCUSDT'})
+    console.log(btccandles)
+    binanceRest.accountSnapshot({type:'SPOT',limit:30, startTime:timestamp, endTime:timestamp2})
     .then(data => {
-        console.log(data);
+        // data.snapshotVos = data.snapshotVos.map(element => element.updateTime = element.updateTime + 1000)
+        data.snapshotVos.forEach((element,index) => {
+            data.snapshotVos[index].updateTime = data.snapshotVos[index].updateTime + 1000
+            data.snapshotVos[index].total = parseFloat(btccandles[btccandles.findIndex((e) => e.openTime == element.updateTime)].open) * element.data.totalAssetOfBtc
+        });
+        console.log(data)
         res.send(data)
+        
     })
     .catch(err => {
         console.error(err);
